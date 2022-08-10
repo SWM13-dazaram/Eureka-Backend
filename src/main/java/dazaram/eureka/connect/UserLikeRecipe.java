2460e88385c0d845cdb89a8d697173828ac1b67a
mo0nl19ht <mool2480@gmail.com>
2022-08-10 11:26:16 +0900

    [OPS-133 / #12]feat: user_like_recipe Entity,Repository, test구현

---
+++ src/main/java/dazaram/eureka/connect/UserLikeRecipe.java
@@ -0,0 +1,43 @@
+package dazaram.eureka.connect;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+
+import dazaram.eureka.BaseTimeEntity;
+import dazaram.eureka.recipe.domain.Recipe;
+import dazaram.eureka.user.domain.User;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class UserLikeRecipe extends BaseTimeEntity {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "user_like_recipe_id")
+	private Long id;
+
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "recipe_id")
+	private Recipe recipe;
+
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "user_id")
+	private User user;
+
+	public static UserLikeRecipe create(Recipe recipe, User user) {
+		return new UserLikeRecipe(null, recipe, user);
+	}
+
+}
---
+++ src/test/java/dazaram/eureka/connect/domain/UserLikeRecipeTest.java
@@ -0,0 +1,56 @@
+package dazaram.eureka.connect.domain;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+import dazaram.eureka.connect.UserLikeRecipe;
+import dazaram.eureka.recipe.RecipeTest;
+import dazaram.eureka.recipe.domain.AiRecipe;
+import dazaram.eureka.recipe.domain.ExistingRecipe;
+import dazaram.eureka.user.domain.Gender;
+import dazaram.eureka.user.domain.User;
+
+class UserLikeRecipeTest extends RecipeTest {
+
+	@Test
+	void ExistingRecipe의_좋아요를_1개_생성합니다() {
+		// given
+		ExistingRecipe existingRecipe = makeExistingRecipe();
+		User user = User.builder()
+			.name("test")
+			.phoneNumber("010-1234-5678")
+			.pushAlarmAllow(false)
+			.profileImage("src/test_profile.jpg")
+			.gender(Gender.M)
+			.build();
+		// when
+		UserLikeRecipe userLikeRecipe = UserLikeRecipe.create(existingRecipe, user);
+		// then
+		assertAll(
+			() -> assertThat(userLikeRecipe.getRecipe()).isEqualTo(existingRecipe),
+			() -> assertThat(userLikeRecipe.getUser()).isEqualTo(user)
+		);
+	}
+
+	@Test
+	void AiRecipe의_좋아요를_1개_생성합니다() {
+		// given
+		AiRecipe aiRecipe = makeAiRecipe(makeExistingRecipe().getId());
+		User user = User.builder()
+			.name("test")
+			.phoneNumber("010-1234-5678")
+			.pushAlarmAllow(false)
+			.profileImage("src/test_profile.jpg")
+			.gender(Gender.M)
+			.build();
+		// when
+		UserLikeRecipe userLikeRecipe = UserLikeRecipe.create(aiRecipe, user);
+		// then
+		assertAll(
+			() -> assertThat(userLikeRecipe.getRecipe()).isEqualTo(aiRecipe),
+			() -> assertThat(userLikeRecipe.getUser()).isEqualTo(user)
+		);
+	}
+}
