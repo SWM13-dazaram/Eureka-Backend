8091c256755624140a47467ba538ebb9d6f1ff65
mo0nl19ht <mool2480@gmail.com>
2022-08-10 17:10:36 +0900

    [OPS-133 / #12]fix: rollback 어노테이션 삭제

--- src/test/java/dazaram/eureka/recipe/repository/RecipeRepositoryTest.java
+++ src/test/java/dazaram/eureka/recipe/repository/RecipeRepositoryTest.java
@@ -13,7 +13,6 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.annotation.Rollback;
 import org.springframework.transaction.annotation.Transactional;
 
 import dazaram.eureka.connect.domain.ReplaceIngredient;
@@ -114,7 +113,6 @@ class RecipeRepositoryTest extends RecipeTest {
 	}
 
 	@Test
-	@Rollback(value = false)
 	void id로_ExistingRecipe을_찾고_대체된_식재료를_조회합니다() {
 		final String replaceIngredientName = "replace";
 		final String missingIngredientName = "missing";
