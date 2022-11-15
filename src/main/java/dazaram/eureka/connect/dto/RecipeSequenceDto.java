9d040578139758bd5f149ca601f94b5d5f5ee00f
jungh8n <jh981113@naver.com>
2022-11-15 22:48:23 +0900

    [OPS-152 / #46] fix: 불필요한 코드 제거

--- src/main/java/dazaram/eureka/connect/dto/RecipeSequenceDto.java
+++ src/main/java/dazaram/eureka/connect/dto/RecipeSequenceDto.java
@@ -1,4 +1,7 @@
-package dazaram.eureka.connect.domain.dto;
+package dazaram.eureka.connect.dto;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import dazaram.eureka.elastic.domain.RecipeSequenceDocument;
 import lombok.AccessLevel;
@@ -10,18 +13,23 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class RecipeSequenceDto {
 	private String content;
-	private int sequence;
+	private Long sequence;
+	private List<Long> highlightIndexList;
 
 	@Builder
-	public RecipeSequenceDto(String content, int sequence) {
+	public RecipeSequenceDto(String content, Long sequence, List<Long> highlightIndexList) {
 		this.content = content;
 		this.sequence = sequence;
+		this.highlightIndexList = highlightIndexList;
 	}
 
 	public static RecipeSequenceDto fromDocument(RecipeSequenceDocument recipeSequenceDocument) {
+		// TODO : highlightIndexes 채우기
+		List<Long> highlightIndexes = new ArrayList<>();
 		return RecipeSequenceDto.builder()
 			.content(recipeSequenceDocument.getContent())
 			.sequence(recipeSequenceDocument.getSequence())
+			.highlightIndexList(highlightIndexes)
 			.build();
 	}
 }
