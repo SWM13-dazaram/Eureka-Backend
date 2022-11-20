9b5cb6b1b9f88643a28b9aceef5f40fb4fcb0295
jungh8n <jh981113@naver.com>
2022-11-20 14:51:33 +0900

    [OPS-154 / #51] fix: 보유 식재료 조회 시 CustomIngredient도 조회하기

--- src/main/java/dazaram/eureka/ingredient/dto/UserIngredientDetailsDto.java
+++ src/main/java/dazaram/eureka/ingredient/dto/UserIngredientDetailsDto.java
@@ -4,8 +4,10 @@ import java.time.LocalDate;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
 
+import dazaram.eureka.ingredient.domain.CustomIngredient;
 import dazaram.eureka.ingredient.domain.UserIngredient;
 import lombok.AccessLevel;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
@@ -24,12 +26,36 @@ public class UserIngredientDetailsDto {
 	private String memo;
 	private BasicIngredientDto ingredient;
 
-	public UserIngredientDetailsDto(UserIngredient userIngredient) {
-		id = userIngredient.getId();
-		name = userIngredient.getName();
-		insertDate = userIngredient.getInsertDate();
-		expireDate = userIngredient.getExpireDate();
-		memo = userIngredient.getMemo();
-		ingredient = BasicIngredientDto.fromEntity(userIngredient.getIngredient());
+	@Builder
+	public UserIngredientDetailsDto(Long id, String name, LocalDate insertDate, LocalDate expireDate, String memo,
+		BasicIngredientDto ingredient) {
+		this.id = id;
+		this.name = name;
+		this.insertDate = insertDate;
+		this.expireDate = expireDate;
+		this.memo = memo;
+		this.ingredient = ingredient;
+	}
+
+	public static UserIngredientDetailsDto fromUserIngredient(UserIngredient userIngredient) {
+		return UserIngredientDetailsDto.builder()
+			.id(userIngredient.getId())
+			.ingredient(BasicIngredientDto.fromEntity(userIngredient.getIngredient()))
+			.name(userIngredient.getName())
+			.memo(userIngredient.getMemo())
+			.insertDate(userIngredient.getInsertDate())
+			.expireDate(userIngredient.getExpireDate())
+			.build();
+	}
+
+	public static UserIngredientDetailsDto fromCustomIngredient(CustomIngredient customIngredient) {
+		return UserIngredientDetailsDto.builder()
+			.id(customIngredient.getId())
+			.name(customIngredient.getName())
+			.memo(customIngredient.getMemo())
+			.insertDate(customIngredient.getInsertDate())
+			.expireDate(customIngredient.getExpireDate())
+			.ingredient(BasicIngredientDto.builder().icon(customIngredient.getIcon()).build())
+			.build();
 	}
 }
